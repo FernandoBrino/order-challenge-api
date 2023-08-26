87d5bd773a725cb82925e7bf8b7d004f1fd50bc2
Fernando Brino <fernandobrino11@gmail.com>
2023-08-25 21:05:58 -0300

    feat: config tests

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// Configura os teste para entender determinas sintaxes do ts;
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+});
