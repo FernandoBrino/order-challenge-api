64fd103b8310fb260382fa92e477c45dd0ba98a7
Fernando Brino <fernandobrino11@gmail.com>
2023-08-25 21:27:26 -0300

    feat: get a order by id service

---
+++ src/services/get-order-by-id.ts
@@ -0,0 +1,30 @@
+import { OrdersRepository } from "@/repositories/orders-repository";
+import { Order } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface GetOrderByIdServiceRequest {
+  id: string;
+}
+
+interface GetOrderByIdServiceResponse {
+  order: Order;
+}
+
+export class GetOrderByIdService {
+  constructor(private ordersRepository: OrdersRepository) {}
+
+  async execute({
+    id,
+  }: GetOrderByIdServiceRequest): Promise<GetOrderByIdServiceResponse> {
+    // Busca um pedido pelo id (número do pedido) enviado
+    const order = await this.ordersRepository.findById(id);
+
+    if (!order) {
+      throw new ResourceNotFoundError();
+    }
+
+    return {
+      order,
+    };
+  }
+}
