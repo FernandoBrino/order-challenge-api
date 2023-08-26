6ccc42fd61b2598318f9680b6014db3f4ca64191
Fernando Brino <fernandobrino11@gmail.com>
2023-08-25 22:08:57 -0300

    feat: delete order service

---
+++ src/services/delete-order.ts
@@ -0,0 +1,19 @@
+import { OrdersRepository } from "@/repositories/orders-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface DeleteOrderServiceRequest {
+  id: string;
+}
+
+export class DeleteOrderService {
+  constructor(private ordersRepository: OrdersRepository) {}
+
+  async execute({ id }: DeleteOrderServiceRequest): Promise<void> {
+    // Busca um pedido pelo id (número do pedido) enviado
+    const order = await this.ordersRepository.delete(id);
+
+    if (order === null) {
+      throw new ResourceNotFoundError();
+    }
+  }
+}
