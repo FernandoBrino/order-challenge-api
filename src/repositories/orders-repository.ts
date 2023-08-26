fac0b3405b7db3cfe2b34702b7acc21b653c2142
Fernando Brino <fernandobrino11@gmail.com>
2023-08-25 21:07:56 -0300

    feat: create orders repository

---
+++ src/repositories/orders-repository.ts
@@ -0,0 +1,8 @@
+import { Order, Prisma } from "@prisma/client";
+
+// Uma abstração dos métodos necessário no repositório de Orders
+export interface OrdersRepository {
+  create(data: Prisma.OrderCreateInput): Promise<Order>;
+  findMany(): Promise<Order[]>;
+  findById(id: string): Promise<Order | null>;
+}
