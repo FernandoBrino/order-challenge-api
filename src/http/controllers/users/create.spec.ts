163bda9f425807056994b125857ee4aa04773b39
Fernando Brino <fernandobrino11@gmail.com>
2023-08-26 18:50:45 -0300

    test(e2e): create user controller

---
+++ src/http/controllers/users/create.spec.ts
@@ -0,0 +1,22 @@
+import { app } from "@/app";
+import request from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+describe("Create User (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create", async () => {
+    const org = await request(app.server).post("/users").send({
+      password: "123456",
+      email: "org@example.com",
+    });
+
+    expect(org.statusCode).toEqual(201);
+  });
+});
