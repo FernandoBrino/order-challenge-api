1c2bdee31585455698c18919ac87b7026f8586b8
Fernando Brino <fernandobrino11@gmail.com>
2023-08-26 14:14:34 -0300

    fix: type request params create order

--- src/repositories/orders-repository.ts
+++ src/repositories/orders-repository.ts
@@ -2,7 +2,7 @@ import { Order, Prisma } from "@prisma/client";
 
 // Uma abstração dos métodos necessário no repositório de Orders
 export interface OrdersRepository {
-  create(data: Prisma.OrderCreateInput): Promise<Order>;
+  create(data: Prisma.OrderUncheckedCreateInput): Promise<Order>;
   findMany(): Promise<Order[]>;
   findById(id: string): Promise<Order | null>;
   delete(id: string): Promise<void | null>;
