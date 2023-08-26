36ed14a9dbeb784bae28b0b70101abfa21d9c11f
Fernando Brino <fernandobrino11@gmail.com>
2023-08-26 15:54:28 -0300

    feat: register jwt plugin

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
+import { env } from "./env";
 
 export const app = fastify();
+
+// Registra o plugin do fastify, passando a data de expiração do token e o secret para encriptar
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: "10m",
+  },
+});
