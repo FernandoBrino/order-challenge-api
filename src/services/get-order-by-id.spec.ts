8a746766559c2d78e32273d069e3d7af5099ea07
Fernando Brino <fernandobrino11@gmail.com>
2023-08-27 17:46:46 -0300

    test(unit): returning order items get order service

--- src/services/get-order-by-id.spec.ts
+++ src/services/get-order-by-id.spec.ts
@@ -3,16 +3,19 @@ import { describe, beforeEach, it, expect } from "vitest";
 import { GetOrderByIdService } from "./get-order-by-id";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { InMemoryItemsRepository } from "@/repositories/in-memory/in-memory-items-repository";
 
 let ordersRepository: InMemoryOrdersRepository;
+let itemsRepository: InMemoryItemsRepository;
 let usersRepository: InMemoryUsersRepository;
 let sut: GetOrderByIdService;
 
 describe("Get Order By Id Service", () => {
   beforeEach(async () => {
     ordersRepository = new InMemoryOrdersRepository();
+    itemsRepository = new InMemoryItemsRepository();
     usersRepository = new InMemoryUsersRepository();
-    sut = new GetOrderByIdService(ordersRepository);
+    sut = new GetOrderByIdService(ordersRepository, itemsRepository);
 
     await usersRepository.create({
       id: "1",
@@ -29,12 +32,28 @@ describe("Get Order By Id Service", () => {
       creationDate: new Date(),
     });
 
-    const { order } = await sut.execute({
+    await itemsRepository.create([
+      {
+        orderId: createdOrder.orderId,
+        productId: "123",
+        quantity: 1,
+        price: 500,
+      },
+    ]);
+
+    const { order, orderItems } = await sut.execute({
       id: createdOrder.orderId,
       userId: "1",
     });
 
     expect(order.orderId).toEqual(expect.any(String));
+    expect(orderItems).toEqual([
+      expect.objectContaining({
+        productId: "123",
+        quantity: 1,
+        price: 500,
+      }),
+    ]);
   });
 
   it("should not be able to find a order with wrong id", async () => {
