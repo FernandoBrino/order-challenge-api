c363aa12f90e0643f7759d43452d0abfbfa7f377
Fernando Brino <fernandobrino11@gmail.com>
2023-08-27 17:46:18 -0300

    fix: returning order items get order controller

--- src/http/controllers/orders/get-order-by-id.ts
+++ src/http/controllers/orders/get-order-by-id.ts
@@ -7,23 +7,33 @@ export async function getOrderById(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
+  // Cria um schema de autenticação para receber os dados corretamente
   const getOrderRouteSchema = z.object({
     id: z.string(),
   });
 
+  // Valida se os dados recebidos seguem o schema de validação
   const { id } = getOrderRouteSchema.parse(request.params);
 
-  const getOrderByIdService = makeGetOrderByIdService();
-
+  // Pega o id do usuário logado
   const { sub } = request.user;
 
+  // Chama o serviço de buscar por um pedido pelo id
+  const getOrderByIdService = makeGetOrderByIdService();
+
   try {
-    const { order } = await getOrderByIdService.execute({ id, userId: sub });
+    // Executa o serviço de buscar por um pedido pelo id
+    const { order, orderItems } = await getOrderByIdService.execute({
+      id,
+      userId: sub,
+    });
 
     return reply.status(200).send({
       order,
+      items: orderItems,
     });
   } catch (error) {
+    // Retorna um erro especifico para dado não encontrado
     if (error instanceof ResourceNotFoundError) {
       return reply.status(404).send();
     }
