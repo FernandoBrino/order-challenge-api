96ef121375c4e40f0364e5a31149e04810e75130
Fernando Brino <fernandobrino11@gmail.com>
2023-08-27 17:42:31 -0300

    feat: add schemas to routes

--- src/http/controllers/orders/routes.ts
+++ src/http/controllers/orders/routes.ts
@@ -3,9 +3,44 @@ import { create } from "./create";
 import { verifyJwt } from "@/http/middlewares/jwt-verify";
 import { getOrderById } from "./get-order-by-id";
 import { fetchAllOrders } from "./fetch-all";
+import { deleteById } from "./delete";
+import { update } from "./update";
+import { createOrderSchema } from "./swagger/create-order-schema";
+import { createOrderByIdSchema } from "./swagger/get-order-by-id-schema";
+import { deleteOrderSchema } from "./swagger/delete-order-schema";
+import { updateOrderByIdSchema } from "./swagger/update-order-schema";
+import { fetchAllOrdersSchema } from "./swagger/fetch-all-orders-schema";
 
 export async function ordersRoutes(app: FastifyInstance) {
-  app.get("/order/:id", { onRequest: [verifyJwt] }, getOrderById);
-  app.get("/order/list", { onRequest: [verifyJwt] }, fetchAllOrders);
-  app.post("/order", { onRequest: [verifyJwt] }, create);
+  app.get(
+    "/order/:id",
+    {
+      onRequest: [verifyJwt],
+      schema: createOrderByIdSchema,
+    },
+    getOrderById
+  );
+  app.get(
+    "/order/list",
+    { onRequest: [verifyJwt], schema: fetchAllOrdersSchema },
+    fetchAllOrders
+  );
+  app.post(
+    "/order",
+    {
+      onRequest: [verifyJwt],
+      schema: createOrderSchema,
+    },
+    create
+  );
+  app.put(
+    "/order/:id",
+    { onRequest: [verifyJwt], schema: updateOrderByIdSchema },
+    update
+  );
+  app.delete(
+    "/order/:id",
+    { onRequest: [verifyJwt], schema: deleteOrderSchema },
+    deleteById
+  );
 }
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,22 @@
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
 import { authenticate } from "./authenticate";
+import { authenticateSchema } from "./swagger/authenticate-schema";
+import { createUserSchema } from "./swagger/create-schema";
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.post("/sessions", authenticate);
-  app.post("/users", create);
+  app.post(
+    "/sessions",
+    {
+      schema: authenticateSchema,
+    },
+    authenticate
+  );
+  app.post(
+    "/users",
+    {
+      schema: createUserSchema,
+    },
+    create
+  );
 }
